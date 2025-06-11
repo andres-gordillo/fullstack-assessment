0a362c774eceeb78d0005e1040c80c991d0e69d5
andres-gordillo <andres.d.gordillo0@gmail.com>
2025-06-11 18:56:02 -0400

    created get request with signals for messages

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -1,7 +1,9 @@
-import { Signal, computed, Injectable } from "@angular/core";
+import { computed, Injectable, signal } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Observable, BehaviorSubject } from "rxjs";
+import { Observable, BehaviorSubject, EMPTY } from "rxjs";
 import { switchMap, catchError } from "rxjs";
+import { Message, CreateMessageResponse } from "../models/message.model";
+import { toSignal} from '@angular/core/rxjs-interop'
 
 
 
@@ -10,5 +12,44 @@ import { switchMap, catchError } from "rxjs";
 })
 
 export class MessageService {
-    
+    private apiUrl = 'http://localhost:3000/main/v1/messages'
+
+
+    // State
+    private messages = signal<Message[]>([]);
+    private error = signal<string | null>(null)
+    private loading = signal(false)
+
+    private dataRefresh$ = new BehaviorSubject<void>(undefined)
+
+    private messagesData = toSignal(
+        this.dataRefresh$.pipe(
+            switchMap(() => {
+                this.loading.set(true)
+                this.error.set(null)
+                return this.http.get<Message[]>(this.apiUrl, {withCredentials: true})
+            }),
+            catchError(e => {
+                this.error.set(e)
+                this.loading.set(false)
+                return EMPTY
+            })
+        ),
+        {initialValue: []}
+    )
+
+    constructor(private http: HttpClient) {
+        const newMessages = this.messagesData()
+
+        this.messages.set(newMessages)
+        this.loading.set(false)
+    }
+
+    pullMessages(): void {
+        this.dataRefresh$.next()
+    }
+
+
+
+
 }
