8065473619e9be712ddc03da29d156e1dbc4ee2d
andres-gordillo <andres.d.gordillo0@gmail.com>
2025-06-11 20:39:18 -0400

    created all components structure

--- frontend/src/app/components/form-component/form-component.component.ts
+++ frontend/src/app/components/form-component/form-component.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
-import { MatInput } from '@angular/material/input';
+import { MatInput, MatFormField, MatLabel } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-form-component',
-  imports: [],
+  imports: [MatInput, MatFormField, MatCard, MatCardContent, MatCardHeader, MatCardTitle, MatButton, MatLabel ],
   templateUrl: './form-component.component.html',
   styleUrl: './form-component.component.scss'
 })
